7a5a9210194e593cd5d22b526c0fc065c0cdc736
jbw <jb@98game.cn>
2019-08-29 15:35:56 +0800

    GridView组件 以及动态GridView

--- flutterdemo/lib/main.dart
+++ flutterdemo/lib/main.dart
@@ -10,29 +10,55 @@ class MyApp extends StatelessWidget {
     // TODO: implement build
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(title: Text('FlutterDemo')),
-      body: HomeContent(),
-    )
-    );
+          appBar: AppBar(title: Text('FlutterDemo')),
+          body: LayoutDemo(),
+        ));
   }
 }
+class LayoutDemo extends StatelessWidget {
 
-class HomeContent extends StatelessWidget {
-  //自定义方法
-  Widget _getListData(context, index) {
-    return ListTile(
-        title: Text(listData[index]["title"]),
-        leading: Image.network(listData[index]["imageUrl"]),
-        subtitle: Text(listData[index]["author"]),
+
+  Widget _getListData (context,index) {
+    return Container(
+      child:Column(
+        children: <Widget>[
+          Image.network(listData[index]['imageUrl']),
+          SizedBox(height: 12),
+          Text(
+            listData[index]['title'],
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 18
+            ),
+          )
+        ],
+
+      ),
+      decoration: BoxDecoration(
+          border: Border.all(
+              color:Color.fromRGBO(233, 233,233, 0.9),
+              width: 1
+          )
+      ),
+
+      // height: 400,  //设置高度没有反应
     );
   }
+
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return ListView.builder(
-        itemCount: listData.length,
-        itemBuilder: this._getListData
+    return GridView.builder(
+      //注意
+      gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisSpacing:10.0 ,   //水平子 Widget 之间间距
+        mainAxisSpacing: 10.0,    //垂直子 Widget 之间间距
+        crossAxisCount: 2,  //一行的 Widget 数量
+//        childAspectRatio:0.9,  //宽度和高度的比例
+      ),
+      padding: EdgeInsets.all(10),
+      itemCount: listData.length,
+      itemBuilder:this._getListData,
     );
   }
 }
