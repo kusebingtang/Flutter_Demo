2d6f6cdd97ac7e905d242baa182b138cc0d79e9d
jbw <jb@98game.cn>
2019-10-17 16:51:42 +0800

    StatefulWidget有状态组件 、页面上绑定数据、改变页面数据 、实现计数器功能

--- flutterdemo/lib/main.dart
+++ flutterdemo/lib/main.dart
@@ -7,59 +7,42 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(title: Text('FlutterDemo')),
-          body: LayoutDemo(),
-        ));
+      appBar: AppBar(title: Text('Flutter Demo')),
+      body: HomePage(),
+    ));
   }
 }
-class LayoutDemo extends StatelessWidget {
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List list = new List();
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Wrap(
-      spacing:10,
-      runSpacing: 10,
-      // direction: Axis.vertical,
-      // alignment:WrapAlignment.spaceEvenly,
-      // runAlignment: WrapAlignment.center,
+    return ListView(
       children: <Widget>[
-        MyButton("第1集"),
-        MyButton("第2集"),
-        MyButton("第3集"),
-        MyButton("第4集"),
-        MyButton("第5集"),
-        MyButton("第6集"),
-        MyButton("第7集"),
-        MyButton("第8集"),
-        MyButton("第9集"),
-        MyButton("第10集"),
-        MyButton("第11集"),
-        MyButton("第3集"),
-        MyButton("第4集"),
-        MyButton("第5集"),
-        MyButton("第6集"),
-        MyButton("第7集"),
-        MyButton("第8集"),
-        MyButton("第9集"),
-        MyButton("第10集"),
-        MyButton("第11集"),
+        Column(
+            children: this.list.map((value) {
+          return ListTile(
+            title: Text(value),
+          );
+        }).toList()),
+        SizedBox(height: 20),
+        RaisedButton(
+          child: Text("按钮"),
+          onPressed: () {
+            setState(() {
+              this.list.add('新增数据1');
+              this.list.add('新增数据2');
+            });
+          },
+        )
       ],
-
-    );
-  }
-}
-
-class MyButton extends StatelessWidget{
-  final String text;
-  const MyButton(this.text,{Key key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return RaisedButton(
-        child: Text(this.text),
-        textColor:Theme.of(context).accentColor,
-        onPressed: (){
-        }
     );
   }
 }
