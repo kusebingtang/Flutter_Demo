c907c84d70e192f015258c817883f34bd495cec8
jbw <jb@98game.cn>
2019-10-24 11:15:29 +0800

    Flutter 轮播图 flutter_swiper

---
+++ Flutter_Demo_28_轮播图 flutter_swiper/lib_2/pages/tabs/Home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(       
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+            RaisedButton(
+              child: Text('跳转到轮播图页面'),
+              onPressed: (){
+                  Navigator.pushNamed(context, '/swiper');
+              },
+            )            
+        ]
+      ),
+    );
+  }
+}
